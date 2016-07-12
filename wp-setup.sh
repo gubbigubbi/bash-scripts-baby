fa26991934615f64bd0a1a8fb8f8629df6b80d89
gubbigubbi <rhys@theitman.net.au>
2016-07-12 11:09:20 +1000

    Updates to wp setup to include blog description, better titles and rewriting permalinks

--- wp-setup.sh
+++ wp-setup.sh
@@ -3,9 +3,19 @@ IFS=","
 echo "Please enter the site name"
 read sitename
 slug=${sitename,,}
-echo $slug
+str=${slug// /}
+echo $str
+echo "Please enter the WP description"
+read description
 wp core download
-wp core config --dbuser=root --dbpass=root --dbname=$slug
+wp core config --dbuser=root --dbpass=root --dbname=$str
 wp db create
-wp core install --url=$slug.local --title=$sitename --admin_user=admin --admin_password=admin --admin_email=rhys@theitman.net.au
+wp core install --url=$str.local --title=$sitename --admin_user=admin --admin_password=admin --admin_email=rhys@theitman.net.au
+wp option update blogdescription $description
+echo "WP ready, removing default plugins and themes"
+wp plugin delete hello
+wp theme delete twentyfourteen twentythirteen
+echo "Setting up permalinks now..."
+wp option update permalink_structure ""
+wp rewrite structure '/%postname%/' --hard
 echo "Setup :)"
